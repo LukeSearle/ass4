a9de5a977834fa23d01c156d49c2fe8f318e7cb0
Luke Searle <luke.searle@student.adfa.edu.au>
2014-06-16 20:35:36 +1000

    Added javadoc, fixed #1 (NullPointerException bug)

--- src/TSP_Heuristic1.java
+++ src/TSP_Heuristic1.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.LinkedList;
@@ -11,6 +10,26 @@ import java.util.LinkedList;
  *
  */
 public class TSP_Heuristic1 extends TSP {
+    /**
+     * The size of the initial population
+     */
+
+    public static final int POPULATION_SIZE = 100;
+
+    /**
+     * The number of generations to run
+     */
+
+    public static final int GENERATIONS = 1000;
+
+    /**
+     * The number of cities to load
+     */
+    public static final int CITIES = 100;
+
+    /**
+     * The number of cities loaded
+     */
     protected int numCities;
 
     /**
@@ -34,18 +53,18 @@ public class TSP_Heuristic1 extends TSP {
     }
 
     /**
-     * Finds a (near) shortest path that starts at the first city, visits
-     * all the other city, then returns to the first city; and stores
-     * this information in the appropriate instance variables.
+     * Finds a (near) shortest path that starts at the first city,
+     * visits all the other city, then returns to the first city; and
+     * stores this information in the appropriate instance variables.
      */
     public LinkedList<Path> findMinPaths() {
         // Initialize population
-        Population pop = new Population(100, true);
+        Population pop = new Population(POPULATION_SIZE, true);
 
         GA g = new GA();
         // Evolve population for 100 generations
         pop = g.evolvePopulation(pop);
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < GENERATIONS; i++) {
             pop = g.evolvePopulation(pop);
         }
 
@@ -61,12 +80,12 @@ public class TSP_Heuristic1 extends TSP {
     public static void main(String[] args) {
         // Test and time the preprocessing component
         Date dateBefore = new Date();
-        TSP bruteForce = new TSP_Heuristic1("cities.txt", 5);
+        TSP bruteForce = new TSP_Heuristic1("cities.txt", CITIES);
         Date dateAfter = new Date();
         long preprocessingTime =
                 dateAfter.getTime() - dateBefore.getTime();
-        System.out.println("Time to preprocess is: " + preprocessingTime +
-                " milliseconds.");
+        System.out.println("Time to preprocess is: " + preprocessingTime
+                + " milliseconds.");
 
         // Test and time the solution generation component
         dateBefore = new Date();
@@ -88,22 +107,35 @@ public class TSP_Heuristic1 extends TSP {
 
             // Display the actual min path(s).
             System.out.println("The minimum path(s): ");
+            int count = 0;
             for (int j = 0; j < minPaths.size(); j++) {
                 Path path = minPaths.get(j);
                 System.out.println(path.toString());
+                for (City c: minPaths.get(j)) if (c != null) count++;
             }
+            System.out.println(count);
         } else {
             System.out.println("Error: no paths generated.");
         }
 
     }
 
+    /**
+     * Class that represents a population of tours to be evolved
+     */
     public class Population {
 
-        // Holds population of tours
+        /**
+         * The population of tours
+         */
         Tour[] tours;
 
-        // Construct a population
+        /**
+         * Construct a new population
+         *
+         * @param populationSize The size of the population
+         * @param initialise Whether to initialise the population
+         */
         public Population(int populationSize, boolean initialise) {
             tours = new Tour[populationSize];
             // If we need to initialise a population of tours do so
@@ -117,17 +149,28 @@ public class TSP_Heuristic1 extends TSP {
             }
         }
 
-        // Saves a tour
+        /**
+         * Add a tour
+         * @param index The position to add the tour to
+         * @param tour The tour to add
+         */
         public void saveTour(int index, Tour tour) {
             tours[index] = tour;
         }
 
-        // Gets a tour from population
+        /**
+         * Return a specific tour
+         * @param index The index of the tour to return
+         * @return The requested tour
+         */
         public Tour getTour(int index) {
             return tours[index];
         }
 
-        // Gets the best tour in the population
+        /**
+         * Get the 'fittest' (shortest) Tour from the population
+         * @return The shortest Tour
+         */
         public Tour getFittest() {
             Tour fittest = tours[0];
             // Loop through individuals to find fittest
@@ -139,25 +182,52 @@ public class TSP_Heuristic1 extends TSP {
             return fittest;
         }
 
-        // Gets population size
+        /**
+         * Gets the size of this population
+         * @return The size of this population
+         */
         public int populationSize() {
             return tours.length;
         }
     }
 
+    /**
+     * A Tour of cities, roughly equivalent to a Path
+     */
     public class Tour {
 
-        // Holds our tour of cities
-        private ArrayList tour = new ArrayList<City>();
-        // Cache
+        /**
+         * The cities of the tour
+         */
+        private ArrayListM tour = new ArrayListM<City>(numCities+1);
+
+        /**
+         * The 'fitness' of the tour
+         */
         private double fitness = 0;
+
+        /**
+         * The distance of the tour
+         */
         private int distance = 0;
 
-        // Constructs a blank tour
+        /**
+         * Default constructor that constructs an empty tour,
+         * filled with null
+         */
         public Tour() {
-            for (int i = 0; i < numCities; i++) {
+            for (int i = 0; i < numCities + 1; i++) {
                 tour.add(null);
             }
+        }
+
+        /**
+         * Constructs a Tour from another instance of LinkedList (can
+         * be used with Path
+         * @param tour The new tour
+         */
+        public Tour(ArrayListM tour) {
+            this.tour = tour;
         }
 
         /**
@@ -168,14 +238,13 @@ public class TSP_Heuristic1 extends TSP {
             tour.add(firstCity);
         }
 
-        public Tour(ArrayList tour) {
-            this.tour = tour;
-        }
-
-        // Creates a random individual
+        /**
+         * Generate a random tour
+         */
         public void generateIndividual() {
-            // Loop through all our destination cities and add them to our tour
-            for (int cityIndex = 0; cityIndex < numCities; cityIndex++) {
+            // Loop through all our destination cities and add them
+            // to our tour
+            for (int cityIndex = 0; cityIndex < numCities; cityIndex++){
                 setCity(cityIndex, cities.get(cityIndex));
             }
             // Randomly reorder the tour
@@ -183,20 +252,32 @@ public class TSP_Heuristic1 extends TSP {
             loop();
         }
 
-        // Gets a city from the tour
+        /**
+         * Gets a City from this tour
+         * @param tourPosition The City's position
+         * @return The requested city
+         */
         public City getCity(int tourPosition) {
             return (City) tour.get(tourPosition);
         }
 
-        // Sets a city in a certain position within a tour
+        /**
+         * Sets a city to be the passed city
+         * @param tourPosition The position to put this city in
+         * @param city The city to insert
+         */
         public void setCity(int tourPosition, City city) {
             tour.set(tourPosition, city);
-            // If the tours been altered we need to reset the fitness and distance
+            // If the tours been altered we need to reset the fitness
+            // and distance
             fitness = 0;
             distance = 0;
         }
 
-        // Gets the tours fitness
+        /**
+         * Gets the Tour's fitness
+         * @return The Tour's fitness
+         */
         public double getFitness() {
             if (fitness == 0) {
                 fitness = 1 / (double) getDistance();
@@ -204,7 +285,10 @@ public class TSP_Heuristic1 extends TSP {
             return fitness;
         }
 
-        // Gets the total distance of the tour
+        /**
+         * Gets the total distance of the tour
+         * @return The total distance
+         */
         public int getDistance() {
             if (distance == 0) {
                 int tourDistance = 0;
@@ -215,20 +299,27 @@ public class TSP_Heuristic1 extends TSP {
                     City fromCity = getCity(cityIndex);
                     // City we're travelling to
                     City destinationCity = getCity(0);
-                    // Check we're not on our tour's last city, if we are set our
-                    // tour's final destination city to our starting city
+                    // Check we're not on our tour's last city,
+                    // if we are set our tour's final destination
+                    // city to our starting city
                     if (cityIndex + 1 < tourSize()) {
                         destinationCity = getCity(cityIndex + 1);
                     }
                     // Get the distance between the two cities
                     if (fromCity != null && destinationCity != null)
-                    tourDistance += fromCity.computeDistanceTo(destinationCity);
+                    tourDistance +=
+                            fromCity.computeDistanceTo(destinationCity);
+                    //else System.out.println("IT WAS NULL");
                 }
                 distance = tourDistance;
             }
             return distance;
         }
 
+        /**
+         * Returns this Tour as a path
+         * @return The tour as a path
+         */
         public Path getAsPath(){
             Path p = new Path();
 
@@ -237,16 +328,28 @@ public class TSP_Heuristic1 extends TSP {
             return p;
         }
 
-        // Get number of cities on our tour
+        /**
+         * Gets the size of this tour
+         * @return The size
+         */
         public int tourSize() {
             return tour.size();
         }
 
-        // Check if the tour contains a city
+        /**
+         * Check if this tour contains a given city
+         * @param city The city to look for
+         * @return Tru if the city was found, otherwise false
+         */
         public boolean containsCity(City city) {
             return tour.contains(city);
         }
 
+        /**
+         * Return this tour as a String of the coordinates of it's
+         * cities
+         * @return
+         */
         @Override
         public String toString() {
             String geneString = "|";
@@ -259,14 +362,29 @@ public class TSP_Heuristic1 extends TSP {
 
     public class GA {
 
-        /* GA parameters */
+        /**
+         * The mutation rate
+         */
         private static final double mutationRate = 0.015;
+
+        /**
+         * The size of each tournament
+         */
         private static final int tournamentSize = 5;
+
+        /**
+         * Whether to enable elitism
+         */
         private static final boolean elitism = true;
 
-        // Evolves a population over one generation
+        /**
+         * Evolve a population by one generation
+         * @param pop The population to be evolved
+         * @return The evolved population
+         */
         public Population evolvePopulation(Population pop) {
-            Population newPopulation = new Population(pop.populationSize(), false);
+            Population newPopulation = new
+                    Population(pop.populationSize(), false);
 
             // Keep our best individual if elitism is enabled
             int elitismOffset = 0;
@@ -276,8 +394,8 @@ public class TSP_Heuristic1 extends TSP {
             }
 
             // Crossover population
-            // Loop over the new population's size and create individuals from
-            // Current population
+            // Loop over the new population's size and create
+            // individuals from current population
             for (int i = elitismOffset;
                  i < newPopulation.populationSize(); i++) {
                 // Select parents
@@ -289,7 +407,8 @@ public class TSP_Heuristic1 extends TSP {
                 newPopulation.saveTour(i, child);
             }
 
-            // Mutate the new population a bit to add some new genetic material
+            // Mutate the new population a bit to add some new genetic
+            // material
             for (int i = elitismOffset;
                  i < newPopulation.populationSize(); i++) {
                 mutate(newPopulation.getTour(i));
@@ -298,7 +417,12 @@ public class TSP_Heuristic1 extends TSP {
             return newPopulation;
         }
 
-        // Applies crossover to a set of parents and creates offspring
+        /**
+         * 'Breed' two Tours together
+         * @param parent1 The first parent Tour
+         * @param parent2 The second parent Tour
+         * @return The child Tour
+         */
         public Tour crossover(Tour parent1, Tour parent2) {
             // Create new child tour
             Tour child = new Tour();
@@ -338,7 +462,10 @@ public class TSP_Heuristic1 extends TSP {
             return child;
         }
 
-        // Mutate a tour using swap mutation
+        /**
+         * Mutate a population
+         * @param tour The Tour to be mutated
+         */
         private void mutate(Tour tour) {
             // Loop through tour cities
             for (int tourPos1 = 0; tourPos1 < tour.tourSize();
@@ -346,7 +473,8 @@ public class TSP_Heuristic1 extends TSP {
                 // Apply mutation rate
                 if (Math.random() < mutationRate) {
                     // Get a second random position in the tour
-                    int tourPos2 = (int) (tour.tourSize() * Math.random());
+                    int tourPos2 = (int) (tour.tourSize() *
+                            Math.random());
 
                     // Get the cities at target position in tour
                     City city1 = tour.getCity(tourPos1);
@@ -359,7 +487,11 @@ public class TSP_Heuristic1 extends TSP {
             }
         }
 
-        // Selects candidate tour for crossover
+        /**
+         * Select Population for tournament
+         * @param pop
+         * @return
+         */
         private Tour tournamentSelection(Population pop) {
             // Create a tournament population
             Population tournament = new Population(tournamentSize, false);
